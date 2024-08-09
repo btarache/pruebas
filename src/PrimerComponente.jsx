ff202ea50e992658faf10539d33e51b061050f17
btarache <benjamintarache@gmail.com>
2024-08-08 21:30:39 -0500

    Variables

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,5 +1,15 @@
+const string = 'Esto es un string'
+const number = 12345
+const array = ['Curso de React', 4]
+const boolean = true
+const fecha = new Date()
+const objeto = { nombre: 'Curso de react', duracion: 4 }
+
+
+
+
 export const PrimerComponente = () => {
     return (
-        <h1>Hola Mundo corregido</h1>
+        <p>{ JSON.stringify(fecha) }</p>  /*  Igual para objeto p/e JSON.stringify(fecha) */
     )
 }
